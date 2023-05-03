fce0d6032a9c4ce22b3806969acc70fcc1b5c476
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2023-05-02 21:13:24 -0300

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,9 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -8,7 +12,7 @@ import entidades.Ponto;
 import model.Model;
 import view.View;
 
-public class Controller {
+public class Controller extends JFrame{
 	
 	View view;
 	Model model;
@@ -18,6 +22,7 @@ public class Controller {
 		view = new View(model.getDesenho());
 			
 		view.adicionaListenerSlide(new MudaSlide());
+		view.adicionaListenerRadio(new EscolheForma());
 	}
 	
 	class MudaSlide implements ChangeListener{
@@ -34,8 +39,35 @@ public class Controller {
 			
 			view.atualiza(desenho);
 			
+			
 		}
 		
+	}
+	
+	class EscolheForma implements ActionListener{
+	
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource()==view.getQuadrado()) {
+				System.out.println("Quadrado");
+			}
+			else if(e.getSource()==view.getTriangulo()) {
+				System.out.println("Triangulo");
+			}
+			else if(e.getSource()==view.getPentagono()) {
+				System.out.println("Pentagono");
+			}
+			else if(e.getSource()==view.getHexagono()) {
+				System.out.println("Hexagono");
+			}
+			else if(e.getSource()==view.getHectagono()) {
+				System.out.println("Hectagono");
+			}
+			else if(e.getSource()==view.getOctogono()) {
+				System.out.println("Octogono");
+			}
+			
+		}
 	}
 	
 
